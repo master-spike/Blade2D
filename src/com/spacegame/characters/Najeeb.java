e0eac8fa412f9d8ea1fd940204d2db6fd7697cda
Ben Hack <2515hack@googlemail.com>
2020-04-18 19:35:34 +0100

    -

--- src/com/spacegame/characters/Najeeb.java
+++ src/com/spacegame/characters/Najeeb.java
@@ -7,7 +7,6 @@ public class Najeeb extends AbstractCharacter {
 	public Najeeb(int x, int y, int size) {
 		super(x, y, size);
 		mShapes.add(new TriElem(x, y, x-size/3, y-size, x+size/3, y-size, 1f, 1f, 1f, 1f, 0));
-		mPlayer = true;
 	}
 	
 	
