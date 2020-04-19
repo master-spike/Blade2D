1f8c471dd16077f9753ff5889fbd7734f585c693
Najeeb Al-Shabibi <najeeb.al.shabibi@gmail.com>
2020-04-19 02:54:10 +0100

    adjusted calculations to allow for infinite mass objects to collide with finite mass objects

--- src/com/spacegame/physics/PhysicsCalc.java
+++ src/com/spacegame/physics/PhysicsCalc.java
@@ -21,7 +21,20 @@ public class PhysicsCalc {
 		if (c1.position.x * c1.velocity.x >= 0) return new Vect2fPair(new Vector2f(0,0), new Vector2f(0,0));
 		System.out.println(c1.velocity.x + " " + c1.velocity.y + " " + c1.position.x + " " + c1.position.y + " ");
 		
-		float impulseMag = 2*c1.velocity.x*c1.mass*c2.mass/(c1.mass + c2.mass);
+		
+		float impulseMag = 0;
+		if(c1.mass != Double.POSITIVE_INFINITY && c2.mass != Double.POSITIVE_INFINITY) {
+			impulseMag = 2*c1.velocity.x*c1.mass*c2.mass/(c1.mass + c2.mass);
+		}
+		else if(c2.mass == Double.POSITIVE_INFINITY && c1.mass != Double.POSITIVE_INFINITY) {
+			impulseMag = 2*c1.velocity.x*c1.mass;
+		}
+		else if (c1.mass == Double.POSITIVE_INFINITY&& c2.mass != Double.POSITIVE_INFINITY) {
+			impulseMag = 2*c1.velocity.x*c2.mass;
+		}
+		else {
+			impulseMag = 0;
+		}
 		Vector2f i1 = new Vector2f(impulseMag, 0);
 		Vector2f i2 = new Vector2f(impulseMag, 0);
 		i1 = Vector2f.scale(i1, -1.0f);
